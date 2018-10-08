ead469853988cd82d1629387a4f74488b3f95eae
Sergey Bakhtoyarov <sergey.bakhtoyarov@gmail.com>
2018-10-08 23:33:41 +0200

    devOps

--- app.js
+++ app.js
@@ -21,7 +21,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/', express.static(path.join(__dirname, 'public')));
+app.use('/', express.static(path.join(__dirname, 'public/index.html')));
+
 app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
