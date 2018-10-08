f9c31c3c35d07e02d078a6ebd9bad8c698401494
Sergey Bakhtoyarov <sergey.bakhtoyarov@gmail.com>
2018-10-08 23:25:59 +0200

    add angular app

--- public/main.js
+++ public/main.js
@@ -734,7 +734,7 @@ Object(_angular_platform_browser_dynamic__WEBPACK_IMPORTED_MODULE_1__["platformB
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /Applications/MAMP/htdocs/sites/InFlowMean/InFlow/src/main.ts */"./src/main.ts");
+module.exports = __webpack_require__(/*! /Users/sergeybakhtoyarov/Documents/InFlow/src/main.ts */"./src/main.ts");
 
 
 /***/ })
--- public/styles.js
+++ public/styles.js
@@ -580,8 +580,8 @@ if(false) {}
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Applications/MAMP/htdocs/sites/InFlowMean/InFlow/node_modules/@angular/material/prebuilt-themes/indigo-pink.css */"./node_modules/@angular/material/prebuilt-themes/indigo-pink.css");
-module.exports = __webpack_require__(/*! /Applications/MAMP/htdocs/sites/InFlowMean/InFlow/src/styles.css */"./src/styles.css");
+__webpack_require__(/*! /Users/sergeybakhtoyarov/Documents/InFlow/node_modules/@angular/material/prebuilt-themes/indigo-pink.css */"./node_modules/@angular/material/prebuilt-themes/indigo-pink.css");
+module.exports = __webpack_require__(/*! /Users/sergeybakhtoyarov/Documents/InFlow/src/styles.css */"./src/styles.css");
 
 
 /***/ })
