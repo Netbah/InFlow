1b5f12819ee2de9b9264345ddd2c19e8ef1c4f48
Sergey Bakhtoyarov <sergey.bakhtoyarov@gmail.com>
2018-10-09 02:01:24 +0200

    hotfix

--- app.js
+++ app.js
@@ -27,6 +27,8 @@ app.use('/', express.static(path.join(__dirname, 'public/index.html')));
 
 app.use('/api', apiRouter);
 
+app.use('/tasks', express.static(path.join(__dirname, 'public/index.html')));
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
